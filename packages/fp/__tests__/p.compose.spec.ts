429809de496b2e4bc0d187478a046d30de5324e3
Mr.Z <zrd0921@qq.com>
2023-06-01 11:08:19 +0800

    feat: fp pipe/compose factory

--- packages/fp/__tests__/p.compose.spec.ts
+++ packages/fp/__tests__/p.compose.spec.ts
@@ -1,7 +1,8 @@
 import * as FP from '@deot/helper-fp';
+import * as R from 'ramda';
 
-const { compose } = FP; 
-describe('compose.ts', () => {
+const { compose, pipe } = FP; 
+describe('compose/pipe.ts', () => {
 	it('base', () => {
 		const add = (x: number) => {
 			return x + 2;
@@ -14,9 +15,13 @@ describe('compose.ts', () => {
 		};
 
 		const composed = compose(add, mul, sub); 
+		const piped = pipe(add, mul, sub); 
 
-		// // ((5 - 10) * 3) + 2 = -13
+		// ((5 - 10) * 3) + 2 = -13
 		expect(composed(5)).toBe(-13);
+
+		// (5 + 2) * 3 - 10 = 11
+		expect(piped(5)).toBe(11);
 
 		// other
 		expect(compose(add)(1)).toBe(3);
@@ -41,6 +46,7 @@ describe('compose.ts', () => {
 
 		const ctx = {
 			composed: compose(add, mul, sub),
+			piped: pipe(add, mul, sub),
 			x: 2,
 			y: 3,
 			z: 10
@@ -48,6 +54,8 @@ describe('compose.ts', () => {
 
 		// ((5 - 10) * 3) + 2 = -13
 		expect(ctx.composed(5)).toBe(-13);
+		// (5 + 2) * 3 - 10 = 11
+		expect(ctx.piped(5)).toBe(11);
 	});
 
 	it('multiple args', () => {
@@ -57,29 +65,51 @@ describe('compose.ts', () => {
 	});
 
 	it('middlewares', () => {
-		let a = (next: any) => (options: any) => {
-			expect(options).toEqual({ b: 1, c: 1 });
-			return next({
-				...options,
-				a: 1
-			});
+		let step = 0;
+		let a = (next: any) => {
+			expect(++step).toEqual(1);
+			return (options: any) => {
+				expect(options).toEqual({ b: 1, c: 1 });
+				expect(++step).toEqual(4);
+				return next({
+					...options,
+					a: 1
+				});
+			};
 		};
-		let b = (next: any) => (options: any) => {
-			expect(options).toEqual({ c: 1 });
-
-			return next({
-				...options,
-				b: 1
-			});
+		let b = (next: any) => {
+			expect(++step).toEqual(2);
+			return (options: any) => {
+				expect(options).toEqual({ c: 1 });
+				expect(++step).toEqual(3);
+				return next({
+					...options,
+					b: 1
+				});
+			};
 		};
 
 		let page = (options: any) => {
+			expect(++step).toEqual(5);
 			return options;
 		};
 
-		let g = compose(b, a)(page);
+		// next = page;
+		// step1: _next = afn = a(page);
+		// step2: __next = bfn = b(afn);
+		let g = R.compose(b, a)(page); // b(a(page))
 
-		expect(g({ c: 1 })).toEqual({ a: 1, b: 1, c: 1 });
-		expect(b(a(page))({ c: 1 })).toEqual({ a: 1, b: 1, c: 1 });
+		// options = { c: 1 };
+		// step3: _options = __next(options) = bfn(options);
+		// step4: __options = _next(_options) = afn(_options);
+		// step5: next(__options) = page(__options);
+		let result = g({ c: 1 });
+
+		/**
+		 * 这样`next => options => any`的设计，虽然是先执行a，再执行b。
+		 * 但是最后执行是bfn, afn, page这样的顺序，这样的两层函数调用，就像是pipe一样
+		 * 所以中间件这样的设计相当于传递
+		 */
+		expect(result).toEqual({ a: 1, b: 1, c: 1 });
 	});
 });
--- packages/fp/src/pipeline/compose.ts
+++ packages/fp/src/pipeline/compose.ts
@@ -1,22 +1,35 @@
-/**
- * 函数组合
- * compose :: (B -> C) -> (A -> B) -> (A -> C)
- * 
- * 参考：https://raw.githubusercontent.com/reduxjs/redux/master/src/compose.ts
- * 1. 增加对this的支持
- * @param {Function[]} funcs ~
- * @returns {Function} ~
- */
-export const compose = (...funcs: Function[]): Function => {
+type Key = 'reduce' | 'reduceRight';
+const factory = (key: Key) => (...funcs: Function[]): Function => {
 	if (funcs.length === 0) {
 		return (arg: any) => arg;
 	}
 	if (funcs.length === 1) {
 		return funcs[0];
 	}
-	return funcs.reduce((a, b) => {
+
+	// 第一个执行的函数可以接受多个值，后面的仅传递一个值
+	return funcs[key]((a, b) => {
 		return function (this: any, ...args: any) {
 			return a.call(this, b.apply(this, args));
 		};
 	});
 };
+
+/**
+ * 函数组合
+ * compose :: (B -> C) -> (A -> B) -> (A -> C)
+ * 中间件的应用
+ * 
+ * 参考：https://raw.githubusercontent.com/reduxjs/redux/master/src/compose.ts
+ * 增加对this的支持
+ * @param {Function[]} funcs ~
+ * @returns {Function} ~
+ */
+export const compose = factory('reduce');
+
+/**
+ * 函数管道
+ * @param {Function[]} funcs ~
+ * @returns {Function} ~
+ */
+export const pipe = factory('reduceRight');
