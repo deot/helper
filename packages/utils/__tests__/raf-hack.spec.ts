87ad51e097fb1d733f28aa40cdbeb93c7c87c423
Mr.Z <zrd0921@qq.com>
2025-12-03 06:49:27 +0800

    feat(utils): add `caf`

--- packages/utils/__tests__/raf-hack.spec.ts
+++ packages/utils/__tests__/raf-hack.spec.ts
@@ -1,14 +1,26 @@
 Object.defineProperty(window, 'requestAnimationFrame', { value: null });
+Object.defineProperty(window, 'cancelAnimationFrame', { value: null });
 
 import { Utils as t } from '@deot/dev-test';
-import * as Utils from '@deot/helper-utils';
 
-describe('raf.ts', () => {
+describe('raf.ts', async () => {
+	const Utils = await import('@deot/helper-utils');
 	it('setTimeout', async () => {
 		expect.assertions(1);
 		Utils.raf(() => {
 			expect(1).toBe(1);
 		});
 		await t.sleep(20);
+	});
+	it('clearTimeout', async () => {
+		expect.assertions(1);
+		let count = 0;
+		const v = Utils.raf(() => {
+			count++;
+		});
+		await t.sleep(1);
+		Utils.caf(v);
+		await t.sleep(20);
+		expect(count).toBe(0);
 	});
 });
